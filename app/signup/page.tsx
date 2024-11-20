434e7837a8a390d53995417aff178f5fb0196c25
Demian Samus <demiansamus2@gmail.com>
2024-11-20 17:19:41 +0200

    Sign up page done

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -19,18 +19,131 @@ import {
   Chip,
   MultiSelect,
 } from "@mantine/core";
+import { useRouter } from 'next/navigation';
 import { DatePickerInput } from "@mantine/dates";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { useForm } from '@mantine/form';
+import { redirect } from "next/dist/server/api-utils";
 
-const topics = ["⛩️ Anime", "⚽ Sport", "🌳 Nature", "🏛️ Architecture"];
+// const topics = ["⛩️ Anime", "⚽ Sport", "🌳 Nature", "🏛️ Architecture"];
+
+function convertDrawingSpeed(speed: string) {
+  switch (speed) {
+    case "slow":
+      return "01:00:00";
+    case "medium":
+      return "00:30:00";
+    case "fast":
+      return "00:15:00";
+    default:
+      return null;
+  }
+}
+
+function sendRegisterRequest(data: any) {
+  console.log(data);
+  return fetch("http://localhost:8000/auth/registration/", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      username: data.username,
+      email: data.email,
+      password1: data.password1,
+      password2: data.password2,
+      first_name: data.first_name,
+      last_name: data.last_name,
+      profile: {
+        date_of_birth: data.dateOfBirth.toISOString().split('T')[0],
+        skill_level: data.skillLevel,
+        time_requirements: convertDrawingSpeed(data.drawingSpeed),
+        topics: data.topics || [],
+      },
+    }),
+  });
+}
 
 export default function SignUpPage() {
+  const router = useRouter();
+
   const theme = useMantineTheme();
-  const [dateOfBirth, setDateOfBirth] = useState<Date | null>(null);
+
+  const form = useForm({
+    mode: "uncontrolled",
+    initialValues: {
+      username: "",
+      email: "",
+      password1: "",
+      password2: "",
+      first_name: "",
+      last_name: "",
+    },
+    validate: (values) => {
+      const errors: any = {};
+      if (active === 0) {
+        if (values.password1 !== values.password2) {
+          errors.password2 = "Passwords do not match";
+        }
+        if (values.email !== "" && !values.email.includes("@")) {
+          errors.email = "Invalid email";
+        }
+
+        if (values.password1.length < 8) {
+          errors.password1 = "Password is too short";
+        }
+        if (!values.password1.match(/[A-Z]/)) {
+          errors.password1 = "Password must contain at least one uppercase letter";
+        }
+        if (!values.password1.match(/[a-z]/)) {
+          errors.password1 = "Password must contain at least one lowercase letter";
+        }
+        if (!values.password1.match(/[0-9]/)) {
+          errors.password1 = "Password must contain at least one number";
+        }
+        
+      }
+
+      return errors;
+    } 
+  },
+);
+
+  const [topics, setTopics] = useState<any[]>([]);
+
+  useEffect(() => {
+    const preprocessTopics = (topics: any[]) => {
+      return topics.map((topic) => {
+        return {
+          label: topic.name,
+          value: `${topic.id}`,
+        };
+      });
+    }
+
+    fetch("http://localhost:8000/api/topics/")
+      .then((response) => response.json())
+      .then((data) => setTopics(preprocessTopics(data)));
+  }, []);
+
+  useEffect(() => {
+    console.log(topics);
+  }
+  , [topics]);
+
   const [skillLevel, setSkillLevel] = useState<string | null>(null);
+  const [drawingSpeed, setDrawingSpeed] = useState<string | null>(null);
+
   const [active, setActive] = useState(0);
   const nextStep = () =>
-    setActive((current) => (current < 2 ? current + 1 : current));
+    setActive((current) => {
+      if (form.validate().hasErrors) {
+        return current;
+      }
+
+      return current < 2 ? current + 1 : current
+    }
+  );
   const prevStep = () =>
     setActive((current) => (current > 0 ? current - 1 : current));
 
@@ -41,6 +154,25 @@ export default function SignUpPage() {
       setSkillLevel(null);
     }
   };
+
+  const handleDrawingSpeedChipClick = (
+    event: React.MouseEvent<HTMLInputElement>
+  ) => {
+    if (event.currentTarget.value === drawingSpeed) {
+      setDrawingSpeed(null);
+    }
+  }
+
+  const handleSubmit = async () => {
+    let resp = await sendRegisterRequest({...form.getValues()})
+    if (resp.ok) {
+      console.log("Success");
+      router.push("/login");
+    } else {
+      let data = await resp.json();
+      console.log(data);
+    }
+  }
 
   return (
     <Center
@@ -65,28 +197,36 @@ export default function SignUpPage() {
                   label="Username"
                   required={true}
                   placeholder="Your username"
+                  key={form.key("username")}
+                  {...form.getInputProps("username")}
                 />
                 <TextInput
                   label="Email"
                   required={true}
                   placeholder="Your email"
+                  key={form.key("email")}
+                  {...form.getInputProps("email")}
                 />
                 <PasswordInput
                   label="Password"
                   required={true}
                   placeholder="Your password"
+                  key={form.key("password")}
+                  {...form.getInputProps("password1")}
                 />
                 <PasswordInput
                   label="Confirm Password"
                   required={true}
                   placeholder="Confirm your password"
+                  key={form.key("confirmPassword")}
+                  {...form.getInputProps("password2")}
                 />
               </Stack>
             </Stepper.Step>
             <Stepper.Step label="Optional" onClick={() => setActive(1)}>
               <Stack>
-                <TextInput label="First Name" placeholder="Your first name" />
-                <TextInput label="Last Name" placeholder="Your last name" />
+                <TextInput label="First Name" placeholder="Your first name" key={form.key("first_name")} {...form.getInputProps("first_name")} />
+                <TextInput label="Last Name" placeholder="Your last name" key={form.key("last_name")} {...form.getInputProps("last_name")} />
               </Stack>
             </Stepper.Step>
             <Stepper.Step label="Personalization" onClick={() => setActive(2)}>
@@ -95,14 +235,32 @@ export default function SignUpPage() {
                   required={true}
                   label="Date of Birth"
                   placeholder="Your date of birth"
-                  value={dateOfBirth}
-                  onChange={setDateOfBirth}
+                  key={form.key("dateOfBirth")}
+                  {...form.getInputProps("dateOfBirth")}
                 />
                 <Group>
                   <Chip.Group
                     multiple={false}
-                    value={skillLevel}
-                    onChange={setSkillLevel}
+                    key={form.key("drawingSpeed")}
+                    {...form.getInputProps("drawingSpeed")}
+                  >
+                    <Text fw={500}>How fast do you draw?</Text>
+                    <Chip value="slow" onClick={handleDrawingSpeedChipClick}>
+                      Slow
+                    </Chip>
+                    <Chip value="medium" onClick={handleSkillLevelChipClick}>
+                      Medium
+                    </Chip>
+                    <Chip value="fast" onClick={handleSkillLevelChipClick}>
+                      Fast
+                    </Chip>
+                  </Chip.Group>
+                </Group>
+                <Group>
+                  <Chip.Group
+                    multiple={false}
+                    key={form.key("skillLevel")}
+                    {...form.getInputProps("skillLevel")}
                   >
                     <Text fw={500}>What's your skill level? <Text span c="red">*</Text></Text>
                     <Chip value="beginner" onClick={handleSkillLevelChipClick}>
@@ -127,6 +285,8 @@ export default function SignUpPage() {
                   searchable
                   required={true}
                   style={{maxWidth: "500px"}}
+                  key={form.key("topics")}
+                  {...form.getInputProps("topics")}
                 />
               </Stack>
             </Stepper.Step>
@@ -136,7 +296,7 @@ export default function SignUpPage() {
               Back
             </Button>
             {active == 2 ? (
-              <Button variant="filled">
+              <Button variant="filled" onClick={() => handleSubmit()}>
                 Submit
               </Button>
             ) : (
